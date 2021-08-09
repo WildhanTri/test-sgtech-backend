e73adc8a8f539cf7b1c3d95f6fb0bf76e319aaf8
WildhanTri <wildhan.triherlian@mogawe.id>
2021-08-10 03:05:12 +0700

    add prod conn

--- conn.ts
+++ conn.ts
@@ -1,5 +1,8 @@
 import mysql from 'mysql2/promise';
 
-export const conn = mysql.createConnection({ host: 'localhost', user: 'root', password: "root", database: 'test-sgtech-db' })
+// DEV
+// export const conn = mysql.createConnection({ host: 'localhost', user: 'root', password: "root", database: 'test-sgtech-db' })
+// PROD
+export const conn = mysql.createConnection({ host: 'eu-cdbr-west-01.cleardb.com', user: 'b5b0b133ecb10e', password: "e8863f28", database: 'heroku_5bc85757d440e75' })
 
 export default conn
